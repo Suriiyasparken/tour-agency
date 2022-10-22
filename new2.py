cb9f94f0f16f951679862089d443d05095abe92d
Suriiyasaprken <suryan26032001@gmail.com>
2022-10-23 01:14:26 +0530

    first commit

---
+++ new2.py
@@ -0,0 +1,298 @@
+def headings():
+    print(''' 
+  
+
+  *************************************************************************************************    
+              .__               __                                                                   
+  ____  _____|  |__   ____   _/  |_  ____  __ _________  _____     ____   ____   ____   ____ ___.__.
+ /  _ \/  ___/  |  \ /  _ \  \   __\/  _ \|  |  \_  __ \ \__  \   / ___\_/ __ \ /    \_/ ___<   |  |
+(  <_> )___ \|   Y  (  <_> )  |  | (  <_> )  |  /|  | \/  / __ \_/ /_/  >  ___/|   |  \  \___\___  |
+ \____/____  >___|  /\____/   |__|  \____/|____/ |__|    (____  /\___  / \___  >___|  /\___  > ____|
+           \/     \/                                          \//_____/      \/     \/     \/\/        
+ ****************************************************************************************************          
+ GMAIL:suryan26032001@gmail.com                                        ADDRESS:
+ PH NUMBER:+916381841641                                                      167,j.j.nagar,pernambut,vellore dt
+           +919786290268
+ *****************************************************************************************************          
+ ''')
+headings()
+
+print("******************")
+print("AVAILABLE CATEGORY")
+print("        OF        ")
+print("       TOUR       ")
+print("******************")
+print("* Honeymoon tour\n* Spirtual tour\n* Adventure tour\n")
+print("available for\n -solo\n -couple\n -family\n -friends\n tours\n")
+print("Rs 2000 discount for couples and family option tour.\n")
+
+tour_category = input("enter the tour category:")
+
+if tour_category=='honeymoon tour' or tour_category=='honeymoon':
+    print("SELECT THE PLACES")
+    print("1)manali,himachal-lovers paradise\n2)alleppey,kerala-venice of the east\n3)leh ledakh,jammu and kashmir\n")
+    print("CHOOSE TRAVELING OPTIONS")
+    print("# bus\n# train\n# aeroplane\n")
+    traveling_option=input("enter the traveling option:")
+    if traveling_option=='bus':
+        print("*VIEW DETAIL*\n")
+
+        print("1)manali,himachal-lovers paradise\n")
+
+        amount='Rs.10,000'
+        days='3 days'
+        food_amount='RS.2,000'
+        total='12,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}\n")
+
+        print("2)alleppey,kerala-venice of the east\n")
+
+        amount1 = 'Rs.8,000'
+        days2 = '2 days'
+        food_amount3 = 'RS.2,000'
+        total4 = '10,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}\n")
+
+        print("3)leh ledakh,jammu and kashmir\n")
+
+        amount5 = 'Rs.8,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '10,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}\n")
+    elif traveling_option=='train':
+        print("*VIEW DETAIL*\n")
+
+        print("1)manali,himachal-lovers paradise\n")
+
+        amount = 'Rs.6,000'
+        days = '2 days'
+        food_amount = 'RS.1,000'
+        total = '7,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}\n")
+
+        print("2)alleppey,kerala-venice of the east\n")
+
+        amount1 = 'Rs.4,000'
+        days2 = '1 days'
+        food_amount3 = 'RS.1,000'
+        total4 = '5,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}\n")
+
+        print("3)leh ledakh,jammu and kashmir\n")
+
+        amount5 = 'Rs.7,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '9,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}\n")
+
+    elif traveling_option=='aeroplane':
+        print("*VIEW DETAIL*\n")
+
+        print("1)manali,himachal-lovers paradise\n")
+
+        amount = 'Rs.15,000'
+        days = '2 days'
+        food_amount = 'RS.2,000'
+        total = '17,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}\n")
+
+        print("2)alleppey,kerala-venice of the east\n")
+
+        amount1 = 'Rs.10,000'
+        days2 = '2 days'
+        food_amount3 = 'RS.2,000'
+        total4 = '12,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}\n")
+
+        print("3)leh ledakh,jammu and kashmir\n")
+
+        amount5 = 'Rs.15,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '17,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}\n")
+
+elif tour_category=='spirtual tour' or tour_category=='spirtual':
+    print("SELECT THE PLACES\n")
+    print("1)osho house,pune\n2)j.krisnamoorthy,madhanapalli,andhra pradesh\n3)sarguru sri brahmma,nilgri temple\n")
+    print("CHOOSE TRAVELING OPTIONS")
+    print("# bus\n# train\n# aeroplane\n")
+    traveling_option = input("enter the traveling option:")
+    if traveling_option == 'bus':
+        print("*VIEW DETAIL*\n")
+
+        print("1)osho house,pune\n")
+
+        amount = 'Rs.10,000'
+        days = '3 days'
+        food_amount = 'RS.2,000'
+        total = '12,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}\n")
+
+        print("2)j.krisnamoorthy,madhanapalli,andhra pradesh\n")
+
+        amount1 = 'Rs.8,000'
+        days2 = '2 days'
+        food_amount3 = 'RS.2,000'
+        total4 = '10,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}\n")
+        print("*VIEW DETAIL*\n")
+
+        print("3)sarguru sri brahmma,nilgri temple\n")
+
+        amount5 = 'Rs.8,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '10,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}\n")
+    elif traveling_option == 'train':
+        print("1)osho house,pune\n")
+
+        amount = 'Rs.6,000'
+        days = '2 days'
+        food_amount = 'RS.1,000'
+        total = '7,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}\n")
+
+        print("2)j.krisnamoorthy,madhanapalli,andhra pradesh\n")
+
+        amount1 = 'Rs.4,000'
+        days2 = '1 days'
+        food_amount3 = 'RS.1,000'
+        total4 = '5,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}\n")
+
+        print("3)sarguru sri brahmma,nilgri temple\n")
+
+        amount5 = 'Rs.7,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '9,000'
+        print( f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}\n")
+    else:
+        print("*VIEW DETAIL*\n")
+
+        print("1)osho house,pune\n")
+
+        amount = 'Rs.15,000'
+        days = '2 days'
+        food_amount = 'RS.2,000'
+        total = '17,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}")
+
+        print("2)j.krisnamoorthy,madhanapalli,andhra pradesh\n")
+
+        amount1 = 'Rs.10,000'
+        days2 = '2 days'
+        food_amount3 = 'RS.2,000'
+        total4 = '12,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}")
+
+        print("3)sarguru sri brahmma,nilgri temple\n")
+
+        amount5 = 'Rs.15,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '17,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}")
+elif tour_category=='spirtual tour' or tour_category=='spirtual':
+    print("SEE THE PLACES")
+    print("1)ladakh\n2)andaman\n3)goava\n")
+    print("CHOOSE TRAVELING OPTIONS")
+    print("# bus\n# train\n# aeroplane\n")
+    traveling_option = input("enter the traveling option:")
+    if traveling_option == 'bus':
+        print("*VIEW DETAIL*\n")
+
+        print("1)ladakh\n")
+
+        amount = 'Rs.10,000'
+        days = '3 days'
+        food_amount = 'RS.2,000'
+        total = '12,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}\n")
+
+        print("2)andaman\n")
+
+        amount1 = 'Rs.8,000'
+        days2 = '2 days'
+        food_amount3 = 'RS.2,000'
+        total4 = '10,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}\n")
+        print("*VIEW DETAIL*\n")
+
+        print("3)goava\n")
+
+        amount5 = 'Rs.8,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '10,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}\n")
+
+    elif traveling_option == 'train':
+        print("*VIEW DETAIL*\n")
+
+        print("1)ladakh\n")
+
+        amount = 'Rs.6,000'
+        days = '2 days'
+        food_amount = 'RS.1,000'
+        total = '7,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}\n")
+
+        print("2)andaman\n")
+
+        amount1 = 'Rs.4,000'
+        days2 = '1 days'
+        food_amount3 = 'RS.1,000'
+        total4 = '5,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}\n")
+
+        print("3)goava\n")
+
+        amount5 = 'Rs.7,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '9,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}\n")
+    else:
+        print("*VIEW DETAIL*\n")
+
+        print("1)ladakh\n")
+
+        amount = 'Rs.15,000'
+        days = '2 days'
+        food_amount = 'RS.2,000'
+        total = '17,000'
+        print(f"traveling amount is {amount}\ntraveling days is {days}\nfood amount is {food_amount}\ntotal amount is {total}")
+
+        print("2)andaman\n")
+
+        amount1 = 'Rs.10,000'
+        days2 = '2 days'
+        food_amount3 = 'RS.2,000'
+        total4 = '12,000'
+        print(f"traveling amount is {amount1}\ntraveling days is {days2}\nfood amount is {food_amount3}\ntotal amount is {total4}")
+
+        print("3)goava\n")
+
+        amount5 = 'Rs.15,000'
+        days6 = '2 days'
+        food_amount7 = 'RS.2,000'
+        total8 = '17,000'
+        print(f"traveling amount is {amount5}\ntraveling days is {days6}\nfood amount is {food_amount7}\ntotal amount is {total8}")
+else:
+    print("type mistake try again")
+    pass
+
+
+
+
+
+
+
+
+
+
